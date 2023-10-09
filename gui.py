db85983b8b998ca0397e2e24c18e631d1fe355fe
sticky <keai21@student.bth.se>
2023-10-09 15:29:15 +0200

    update

--- gui.py
+++ gui.py
@@ -127,13 +127,13 @@ class TrainStation(ctk.CTk):
         if not modbus_data_queue.empty():
             data = modbus_data_queue.get_nowait()
 
-            match data[1]:
+            match data[0]:
                 case "A":
-                    train_station_hmi.add_train_to_timetable(int(data[0]), data[2:]) # 1, 'Train 1', '09:00', 'Track 1')
+                    train_station_hmi.add_train_to_timetable(int(data[1]), data[2:]) # 1, 'Train 1', '09:00', 'Track 1')
                 case "R":
-                    train_station_hmi.remove_train_from_timetable(int(data[0]))
+                    train_station_hmi.remove_train_from_timetable(int(data[1]))
                 case "T":
-                    train_station_hmi.update_data_tracks(int(data[0]), data[2])
+                    train_station_hmi.update_data_tracks(int(data[1]), data[2])
                     pass
 
         self.after(1000, self.process_modbus_data)
