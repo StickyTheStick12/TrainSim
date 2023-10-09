57ca7cdde86d26247d642cf6310f1ab7164de566
sticky <keai21@student.bth.se>
2023-10-09 15:00:58 +0200

    update

--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
-from tkinter import *
-import customtkinter
+import tkinter as tk
+import tkinter.font as tkfont
+import customtkinter as ctk
+
 import asyncio
 from pymodbus.client import AsyncModbusTlsClient
 from pymodbus.transaction import ModbusTlsFramer
@@ -13,118 +15,135 @@ _logger = logging.getLogger(__file__)
 _logger.setLevel("DEBUG")
 
 # Modbus variables
-datastore_size = 41  # needs to be the same size as the server
-path_to_cert = "cert.perm"
-path_to_key = "key.perm"
-host = ""
+datastore_size = 41  # needs to be the same size as the server, max 125 though
+path_to_cert = "/home/vboxuser/tls/cert.pem"
+path_to_key = "/home/vboxuser/tls/key.pem"
+host = "localhost"
 port = 12345
 
-# TODO: get correct index from modbus
-# TODO: fix modbus read data
 
-class TrainStationHMI(customtkinter.CTk):
+class TrainStation(ctk.CTk):
     def __init__(self):
         super().__init__()
-        self.title("Train Station HMI")
-        customtkinter.set_appearance_mode("dark")
-        self.geometry('800x400')
+        ctk.set_appearance_mode("dark")
+        self.geometry("800x800")
+        self.title("Train station")
 
-        for c in range(3):
-            self.columnconfigure(index=c, weight=1)
+        for x in range(5):
+            self.grid_columnconfigure(x, weight=1)
+            self.grid_rowconfigure(x, weight=1)
 
-        for r in range(2):
-            self.rowconfigure(index=r, weight=1)
+        self.title_font = ctk.CTkFont(size=24)
+        self.subtitle_font = ctk.CTkFont(size=20)
+        self.text_font = ctk.CTkFont(size=14)
 
-        self.track1_label = customtkinter.CTkLabel(self, text="Track 1", fg_color="gray30", corner_radius=6)
-        self.track1_label.grid(row=0, column=0, sticky="nwe", padx=(10, 0), pady=(100, 50))
+        self.timetable_frame = ctk.CTkFrame(self, fg_color="blue")
+        self.timetable_frame.grid(row=0, column=0, sticky="nsew")
 
-        self.track2_label = customtkinter.CTkLabel(self, text="Track 2", fg_color="gray30", corner_radius=6)
-        self.track2_label.grid(row=1, column=0, sticky="nwe", padx=(10, 1), pady=(100, 50))
+        self.tracks_frame = ctk.CTkFrame(self, fg_color="pink")
+        self.tracks_frame.grid(row=0, column=1, sticky="nsew")
 
-        self.track1_rect = customtkinter.CTkCanvas(self, width=100, height=50, highlightthickness=0, bg="green")
-        self.track1_rect.grid(row=0, column=1, pady=(1, 5), padx=(0, 10), sticky="w")
+        self.timetable_data = []
+        self.track_data = []
 
-        self.track2_rect = customtkinter.CTkCanvas(self, width=100, height=50, highlightthickness=0, bg="red")
-        self.track2_rect.grid(row=1, column=1, pady=(5, 10), padx=(0, 10), sticky="w")
+        self.track1_indicator = ctk.CTkCanvas(self.tracks_frame, bg="green", width=100, height=50, highlightthickness=0)
+        self.track2_indicator = ctk.CTkCanvas(self.tracks_frame, bg="green", width=100, height=50, highlightthickness=0)
 
-        self.timetable_frame = customtkinter.CTkFrame(self, width=10)
-        self.timetable_frame.grid(row=0, column=2, rowspan=2, sticky="nsew")
+        self.create_timetable_layout()
+        self.create_track_layout()
 
-        self.timetable_title = customtkinter.CTkLabel(self.timetable_frame, text="Timetable", fg_color="gray30", corner_radius=6, font=("Arial", 14))
-        self.timetable_title.grid(row=0, column=0, columnspan=3, sticky="nwe", pady=(10, 5))
+        self.add_data_timetable(["Train 2", "Track 1", "11:00"])
+        self.add_data_timetable(["Train 1", "Track 1", "11:00"])
+        self.remove_data_timetable(0)
 
-        self.train_data = []  # List to store train data
+        self.update_data_tracks(2, "red")
 
-        self.train_number_label = customtkinter.CTkLabel(self.timetable_frame, text="Train Number ", font=("Arial", 12), anchor="w")
-        self.train_number_label.grid(row=1, column=0, sticky="ew", padx=(10, 0))
+    def create_timetable_layout(self):
+        timetable_label = ctk.CTkLabel(self.timetable_frame, text="Timetable", font=self.title_font,
+                                       corner_radius=6)
 
-        self.departure_time_label = customtkinter.CTkLabel(self.timetable_frame, text="Departure Time ", font=("Arial", 12), anchor="w")
-        self.departure_time_label.grid(row=1, column=1, sticky="ew")
+        train_label = ctk.CTkLabel(self.timetable_frame, text="Train", font=self.subtitle_font, corner_radius=3)
+        track_label = ctk.CTkLabel(self.timetable_frame, text="Track", font=self.subtitle_font, corner_radius=3)
+        departure_label = ctk.CTkLabel(self.timetable_frame, text="Departure", font=self.subtitle_font, corner_radius=3)
 
-        self.track_number_label = customtkinter.CTkLabel(self.timetable_frame, text="Track Number", font=("Arial", 12), anchor="w")
-        self.track_number_label.grid(row=1, column=2, sticky="ew", padx=(0, 10))
+        timetable_label.grid(row=0, column=0, columnspan=3, padx=150, pady=20)
+        train_label.grid(row=1, column=0, padx=20)
+        track_label.grid(row=1, column=1)
+        departure_label.grid(row=1, column=2)
 
+    def create_track_layout(self):
+        track1_label_title = ctk.CTkLabel(self.tracks_frame, text="Track1", font=self.title_font, corner_radius=3)
+        track2_label_title = ctk.CTkLabel(self.tracks_frame, text="Track2", font=self.title_font, corner_radius=3)
 
-    def add_train_to_timetable(self, train_number, departure_time, track_number):
-        idx = len(self.train_data)  # Get the current number of trains
+        track1_label_title.grid(row=0, column=3, columnspan=1, padx=50, pady=20)
+        track2_label_title.grid(row=0, column=4, columnspan=1, padx=50, pady=20)
 
-        self.train_data.append({'train_number': train_number, 'departure_time': departure_time, 'track_number': track_number})
+        self.track1_indicator.grid(row=1, column=3, columnspan=1, padx=50, pady=20)
+        self.track2_indicator.grid(row=1, column=4, columnspan=1, padx=50, pady=20)
 
-        train_number_label = customtkinter.CTkLabel(self.timetable_frame, text=f"{train_number}", font=("Arial", 12), anchor="w")
-        train_number_label.grid(row=idx+2, column=0, sticky="ew", padx=(10, 0))
+    def add_data_timetable(self, index, data):
+        self.timetable_data.insert(index, data)
+        current_row = len(self.timetable_data)
+        train_label = ctk.CTkLabel(self.timetable_frame, text=data[0], font=self.text_font)
+        track_label = ctk.CTkLabel(self.timetable_frame, text=data[1], font=self.text_font)
+        departure_label = ctk.CTkLabel(self.timetable_frame, text=data[2], font=self.text_font)
 
-        departure_time_label = customtkinter.CTkLabel(self.timetable_frame, text=f"{departure_time}", font=("Arial", 12), anchor="w")
-        departure_time_label.grid(row=idx+2, column=1, sticky="ew")
+        train_label.grid(row=current_row + 2, column=0, padx=20)
+        track_label.grid(row=current_row + 2, column=1)
+        departure_label.grid(row=current_row + 2, column=2)
 
-        track_number_label = customtkinter.CTkLabel(self.timetable_frame, text=f"{track_number}", font=("Arial", 12), anchor="w")
-        track_number_label.grid(row=idx+2, column=2, sticky="ew", padx=(0, 10))
+    def remove_data_timetable(self, index):
+        if index < len(self.timetable_data):
+            self.timetable_data.pop(index)
+            for obj in self.timetable_frame.winfo_children():
+                obj.grid_forget()
+            self.create_timetable_layout()
+            for current_row, data in enumerate(self.timetable_data, start=2):
+                train, track, departure = data
+                train_label = ctk.CTkLabel(self.timetable_frame, text=train, font=self.text_font)
+                track_label = ctk.CTkLabel(self.timetable_frame, text=track, font=self.text_font)
+                departure_label = ctk.CTkLabel(self.timetable_frame, text=departure, font=self.text_font)
 
+                train_label.grid(row=current_row, column=0, padx=20)
+                track_label.grid(row=current_row, column=1)
+                departure_label.grid(row=current_row, column=2)
 
-    def remove_train_from_timetable(self, idx):
-        if 0 <= idx < len(self.train_data):
-            del self.train_data[idx]
-            # Clear all widgets in the timetable frame
+    def update_data_tracks(self, track_number, status):
+        if track_number == 1:
+            track_status = self.track1_indicator
+        elif track_number == 2:
+            track_status = self.track2_indicator
+        else:
+            print(f"Invalid track number: {track_number}")
+            return
 
-            for widget in self.timetable_frame.winfo_children():
-                widget.grid_forget()
-
-            # Rebuild the timetable
-            self.timetable_title.grid(row=0, column=0, columnspan=4, sticky="nwe", pady=(10, 5))
-            self.train_number_label.grid(row=1, column=0, sticky="ew", padx=(10, 0))
-            self.departure_time_label.grid(row=1, column=1, sticky="ew")
-            self.track_number_label.grid(row=1, column=2, sticky="ew", padx=(0, 10))
-
-            for i, data in enumerate(self.train_data):
-                train_number_label = customtkinter.CTkLabel(self.timetable_frame, text=data['train_number'], font=("Arial", 12), anchor="w")
-                train_number_label.grid(row=i + 2, column=0, sticky="ew", padx=(10, 0))
-
-                departure_time_label = customtkinter.CTkLabel(self.timetable_frame, text=data['departure_time'], font=("Arial", 12), anchor="w")
-                departure_time_label.grid(row=i + 2, column=1, sticky="ew")
-
-                track_number_label = customtkinter.CTkLabel(self.timetable_frame, text=data['track_number'], font=("Arial", 12), anchor="w")
-                track_number_label.grid(row=i + 2, column=2, sticky="ew", padx=(0, 10))
+        if status == "Occupied":
+            track_status.config(bg="red")
+        else:
+            track_status.config(bg="green")
 
 
     def process_modbus_data(self) -> None:
-        try:
+        if not modbus_data_queue.empty():
             data = modbus_data_queue.get_nowait()
-        except Queue.Empty:
-            data = None
 
-        if data:
-            match data[0]:
+            match data[1]:
                 case "A":
-                    train_station_hmi.add_train_to_timetable('Train 1', '09:00', 'Track 1')
+                    train_station_hmi.add_train_to_timetable(int(data[0]), data[2:]) # 1, 'Train 1', '09:00', 'Track 1')
                 case "R":
-                    train_station_hmi.remove_train_from_timetable(0)
+                    train_station_hmi.remove_train_from_timetable(int(data[2]))
                 case "T":
+                    train_station_hmi.update_data_tracks(int(data[2]), data[3])
                     pass
 
         self.after(1000, self.process_modbus_data)
 
+
 def modbus_client_thread(queue) -> None:
     """This thread will start the modbus client and connect to the server"""
     client = None
+
+    loop = asyncio.new_event_loop()
 
     async def run_client() -> None:
         """Run client"""
@@ -132,7 +151,7 @@ def modbus_client_thread(queue) -> None:
         # ssl_context = ssl.create_default_context()
         # ssl_context.load_cert_chain(certfile="rootCA.pm", keyfile="rootCA.key")  # change to file path
 
-        client = await AsyncModbusTlsClient(
+        client = AsyncModbusTlsClient(
             host,
             port=port,
             framer=ModbusTlsFramer,
@@ -167,9 +186,11 @@ def modbus_client_thread(queue) -> None:
 
                     if hold_register.isError():
                         _logger.error("Error reading holding register")
-
+                    # 10 1 gffff 15:15 1
                     amount_to_read = hold_register.registers[0]
-                    data = "".join([chr(char) for char in hold_register.registers[1:amount_to_read]]).split(" ")
+                    idx = hold_register.registers[1]
+                    data = [idx] + "".join([chr(char) for char in hold_register.registers[2:2+amount_to_read]]).split(" ")
+                    _logger.debug(f"received {data}")
                     _logger.debug("Resetting flag")
                     client.write_register(datastore_size-2, 1, slave=1)
 
@@ -188,8 +209,10 @@ def modbus_client_thread(queue) -> None:
             _logger.info("Keyboard interrupt received. Exiting.")
             client.close()
 
-    asyncio.get_event_loop().run_until_complete(run_client())
-    asyncio.get_event_loop().run_until_complete(read_holding_register())
+    print(path_to_cert)
+
+    loop.run_until_complete(run_client())
+    loop.run_until_complete(read_holding_register())
 
 if __name__ == "__main__":
     modbus_data_queue = Queue()
@@ -197,7 +220,7 @@ if __name__ == "__main__":
     modbus_thread = threading.Thread(target=modbus_client_thread, args=(modbus_data_queue, ))
     modbus_thread.start()
     # Initialize the Train Station HMI
-    train_station_hmi = TrainStationHMI()
+    train_station_hmi = TrainStation()
     train_station_hmi.after(1000, train_station_hmi.process_modbus_data)
 
     try:
