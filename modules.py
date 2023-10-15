69d7df1f6db1657ee20357d571e38774676b1a3e
sticky <keai21@student.bth.se>
2023-10-15 17:42:44 +0200

    testing tls sql

--- modules.py
+++ modules.py
@@ -1,13 +1,22 @@
 import mysqlx
 from mysqlx.errors import DatabaseError
 # Connect to server on localhost, close session also defined here
-session = mysqlx.get_session({
+tls_options = {
+    'ssl-mode': 'REQUIRED',
+    'key': "/home/vboxuser/tls/key.pem",    # Your client's private key
+    'cert': "/home/vboxuser/tls/cert.pem",  # Your client's certificate
+    'ca': "/home/vboxuser/tls/cert.pem",    # Your server's certificate (which is also the CA certificate in this case)
+}
+
+mysql_options = {
     'host': 'localhost',
     'port': 33060,
     'user': 'debian-sys-maint',
     'password': 'RocmET1Ek1eikelK',
-    'schema': 'tssc'
-})
+    'schema': 'tssc',
+}
+
+session = get_session(mysql_options, **tls_options)
 
 
 def closeSession():
