5bcd839def7e852619af4cc400c5078c79c15042
kevin wigren <89857252+StickyTheStick12@users.noreply.github.com>
2023-12-07 12:38:20 +0100

    Update attack.sh

--- attack/attack.sh
+++ attack/attack.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# Define the target directory relative to the script
+parentdir=$(dirname $(pwd))
 target_dir="simulation/scripts"
-
-# Construct the absolute path to the target directory (/a/b/scripts)
-absolute_path="$script_dir/$target_dir"
+python_files_directory="$parentdir/$target_dir"
 
 # Specify the target file
 target_file="switch.py"
 
-# Use sed to replace port numbers from 12345 to 5002 and 12344 to 5010 in the target file
-sed -i 's/\b12345\b/5002/g' "$absolute_path/$target_file"
-sed -i 's/\b12344\b/5010/g' "$absolute_path/$target_file"
+# Use sed to replace port numbers from 12345 to 5002 and 12344 to 5010 in the t>
+sed -i 's/\b12345\b/5002/g' "$python_files_directory/$target_file"
+sed -i 's/\b12344\b/5010/g' "$python_files_directory/$target_file"
