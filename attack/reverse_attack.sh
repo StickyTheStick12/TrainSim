502bad40ffa1c03d18ece2879c1b8552e333b16a
kevin wigren <89857252+StickyTheStick12@users.noreply.github.com>
2023-12-12 15:20:44 +0100

    Update reverse_attack.sh

--- attack/reverse_attack.sh
+++ attack/reverse_attack.sh
@@ -5,7 +5,7 @@ target_dir="simulation/scripts"
 python_files_directory="$parentdir/$target_dir"
 
 # Specify the target file
-target_file="switch.py"
+target_file="hmi.py"
 
 # Use sed to replace port numbers from 5002 to 12345 and 5010 to 12344 in the target file
 sed -i 's/\b5002\b/12345/g' "$python_files_directory/$target_file"
