61c25a3aae7106f88301beff1fb2ac8b7c5a829f
kevin wigren <89857252+StickyTheStick12@users.noreply.github.com>
2023-12-12 15:13:15 +0100

    Update attack.sh

--- attack/attack.sh
+++ attack/attack.sh
@@ -5,7 +5,7 @@ target_dir="simulation/scripts"
 python_files_directory="$parentdir/$target_dir"
 
 # Specify the target file
-target_file="switch.py"
+target_file="hmi.py"
 
 # Use sed to replace port numbers from 12345 to 5002 and 12344 to 5010 in the t>
 sed -i 's/\b12345\b/5002/g' "$python_files_directory/$target_file"
